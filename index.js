74ac613e1d0db41b206624c052310109395c46a1
Igor <igordg@mail.ru>
2020-02-10 03:24:22 +0300

    Revert "Git-effect 1.0.0"
    This reverts commit 9fbbe4f60853c91851dbc61dfdb4ce3126aade4f.

--- index.js
+++
@@ -1,9 +0,0 @@
-const Calendar = require("./Calendar")
-
-function gitEffect(usersText, language){
-    let gitCalendar = new Calendar(usersText, language)
-    gitCalendar.calendar()
-}
-
-module.exports = gitEffect;
-
